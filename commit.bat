3cb008703902d1a1ab10e552ea3f790f4a2376a6
Zhi You Tan <zyt10@uclive.ac.nz>
2017-04-06 17:13:56 +1200

    added jump robot and volleyball robot

--- commit.bat
+++ commit.bat
@@ -1,4 +1,7 @@
 @echo off
+git log -2 --oneline
+echo.
 git status
+echo.
 set /p commit="Commit message: "
 git add -A && git commit -am "%commit%" && git push
--- robot-world/robot-world/Source.cpp
+++ robot-world/robot-world/Source.cpp
@@ -8,117 +8,105 @@
 //#include <stdlib.h>
 #include <math.h>
 #include <GL/freeglut.h>
+#include "loadTGA.h"
 
 GLUquadric *q;    //Required for creating cylindrical objects
-GLuint txId[2];		//Texture ids
+GLuint txId[1];		//Texture ids
 double theta = -10.5;
 int robotMovement = 0;
-bool movementFlag = true;
+bool flag = true;
 float lgt_src[4] = { 0.0f, 50.0f, 0.0f, 1.0f };
-float eye_x = 12, eye_y = 1, eye_z = 12;		//Initial camera position
-float look_x = 12, look_y = 1, look_z = 10;		//"Look-at" point along -z direction
-float theta = 0;		//Look angle
+float eye_x = 12, eye_y = 10, eye_z = 12;		//Initial camera position
+float look_x = 12, look_y = 10, look_z = 10;		//"Look-at" point along -z direction
+float lookTheta = 0;		//Look angle
 int step = 0;		//camera motion
+float angle_up_down = 1.0;
+int cam_hgt = 150;
+float angle_left_right = 0.0;
+
+// jumping robot inits
+float jumpHandAngle = 0;
+float jumpLegAngle = 0;
+int jumpHeight = 0;
+bool jumpUpBool = true;
+
+// volleyball robot inits
+float volleyballHand = 0;
+int volleyballHeight = 0;
+bool volleyballUpBool = true;
 
 void loadTexture()
 {
-	glGenTextures(2, txId); 	// Create 2 texture ids
+	glGenTextures(1, txId); 	// Create 1 texture id
 
 	glBindTexture(GL_TEXTURE_2D, txId[0]);  //Use this texture
-	loadTGA("Wall.tga");
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	//Set texture parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	glBindTexture(GL_TEXTURE_2D, txId[1]);  //Use this texture
 	loadTGA("Floor.tga");
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	//Set texture parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 }
 
-//-- Ground Plane --------------------------------------------------------
 void floor()
 {
-	float white[4] = { 1., 1., 1., 1. };
-	float black[4] = { 0 };
-	glColor4f(0.7, 0.7, 0.7, 1.0);  //The floor is gray in colour
-	glNormal3f(0.0, 1.0, 0.0);
-
-	glMaterialfv(GL_FRONT, GL_SPECULAR, black);
-
-	//The floor is made up of several tiny squares on a 200x200 grid. Each square has a unit size.
-	glBegin(GL_QUADS);
-	for (int i = -200; i < 200; i++)
-	{
-		for (int j = -200; j < 200; j++)
-		{
-			glVertex3f(i, 0.0, j);
-			glVertex3f(i, 0.0, j + 1);
-			glVertex3f(i + 1, 0.0, j + 1);
-			glVertex3f(i + 1, 0.0, j);
-		}
-	}
-	glEnd();
-	glMaterialfv(GL_FRONT, GL_SPECULAR, white);
-
-}
-
-//------- Rail Track ----------------------------------------------------
-// A single circular track of specified radius
-void track(float radius)
-{
-	float angle1, angle2, ca1, sa1, ca2, sa2;
-	float x1, z1, x2, z2, x3, z3, x4, z4;  //four points of a quad
-	float toRad = 3.14159265 / 180.0;  //Conversion from degrees to radians
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, txId[0]);
 
 	glBegin(GL_QUADS);
-	for (int i = 0; i < 360; i += 5)    //5 deg intervals
-	{
-		angle1 = i * toRad;       //Computation of angles, cos, sin etc
-		angle2 = (i + 5) * toRad;
-		ca1 = cos(angle1); ca2 = cos(angle2);
-		sa1 = sin(angle1); sa2 = sin(angle2);
-		x1 = (radius - 0.5)*sa1; z1 = (radius - 0.5)*ca1;
-		x2 = (radius + 0.5)*sa1; z2 = (radius + 0.5)*ca1;
-		x3 = (radius + 0.5)*sa2; z3 = (radius + 0.5)*ca2;
-		x4 = (radius - 0.5)*sa2; z4 = (radius - 0.5)*ca2;
+	//for (int i = -200; i < 200; i++)
+	//{
+	//	for (int j = -200; j < 200; j++)
+	//	{
+	//		glVertex3f(i, 0.0, j);
+	//		glVertex3f(i, 0.0, j++);
+	//		glVertex3f(i + 1, 0.0, j++);
+	//		glVertex3f(i + 1, 0.0, j);
+	//	}
+	//}
 
-		glNormal3f(0., 1., 0.);       //Quad 1 facing up
-		glVertex3f(x1, 1.0, z1);
-		glVertex3f(x2, 1.0, z2);
-		glVertex3f(x3, 1.0, z3);
-		glVertex3f(x4, 1.0, z4);
-
-		glNormal3f(-sa1, 0.0, -ca1);   //Quad 2 facing inward
-		glVertex3f(x1, 0.0, z1);
-		glVertex3f(x1, 1.0, z1);
-		glNormal3f(-sa2, 0.0, -ca2);
-		glVertex3f(x4, 1.0, z4);
-		glVertex3f(x4, 0.0, z4);
-
-		glNormal3f(sa1, 0.0, ca1);   //Quad 3 facing outward
-		glVertex3f(x2, 1.0, z2);
-		glVertex3f(x2, 0.0, z2);
-		glNormal3f(sa2, 0.0, ca2);
-		glVertex3f(x3, 0.0, z3);
-		glVertex3f(x3, 1.0, z3);
-	}
+		glTexCoord2f(0, 150); glVertex3f(-200, 0, -200);
+		glTexCoord2f(0, 0); glVertex3f(-200, 0, 200);
+		glTexCoord2f(150, 0); glVertex3f(200, 0, 200);
+		glTexCoord2f(150, 150); glVertex3f(200, 0, -200);
 	glEnd();
+	glDisable(GL_TEXTURE_2D);
 }
 
-//-------- Tracks  ----------------------------------------------------
-void tracks()
-{
-	glColor4f(0.0, 0.0, 0.3, 1.0);
-	track(115.0);   //Inner track has radius 115 units
-	track(125.0);   //Outer track has radius 125 units
-}
+//-- Ground Plane --------------------------------------------------------
+//void floor()
+//{
+//	glEnable(GL_TEXTURE_2D);
+//	glBindTexture(GL_TEXTURE_2D, txId[0]);
+//
+//	float white[4] = { 1., 1., 1., 1. };
+//	float black[4] = { 0 };
+//	glColor4f(0.7, 0.7, 0.7, 1.0);  //The floor is gray in colour
+//	glNormal3f(0.0, 1.0, 0.0);
+//
+//	glMaterialfv(GL_FRONT, GL_SPECULAR, black);
+//
+//	//The floor is made up of several tiny squares on a 200x200 grid. Each square has a unit size.
+//	glBegin(GL_QUADS);
+//	for (int i = -200; i < 200; i++)
+//	{
+//		for (int j = -200; j < 200; j++)
+//		{
+//			glVertex3f(i, 0.0, j);
+//			glVertex3f(i, 0.0, j + 1);
+//			glVertex3f(i + 1, 0.0, j + 1);
+//			glVertex3f(i + 1, 0.0, j);
+//		}
+//	}
+//	glEnd();
+//	glMaterialfv(GL_FRONT, GL_SPECULAR, white);
+//
+//}
 
 //--Draws a character model constructed using GLUT objects ------------------
-void drawModel()
+void drawModel(void)
 {
+	glDisable(GL_TEXTURE_2D);
+
 	glColor3f(1., 0.78, 0.06);		//Head
 	glPushMatrix();
 		glTranslatef(0, 7.7, 0);
@@ -173,9 +161,192 @@ void drawModel()
 	glPopMatrix();
 }
 
+void myTimer(int value)
+{
+	theta += 2;
+	glutPostRedisplay();
+	glutTimerFunc(50, myTimer, 0);
+}
+
+void robotTimer(int value)
+{
+	if (flag && robotMovement < 30)
+	{
+		robotMovement++;
+	}
+	else if (flag)
+	{
+		flag = !flag;
+		robotMovement--;
+	}
+	else if (!flag && robotMovement > -30)
+	{
+		robotMovement--;
+	}
+	else
+	{
+		flag = !flag;
+		robotMovement++;
+	}
+	glutPostRedisplay();
+	glutTimerFunc(50, robotTimer, 0);
+}
+
+void drawJumpingRobot(void)
+{
+	glDisable(GL_TEXTURE_2D);
+
+	glColor3f(1., 0.78, 0.06);		//Head
+	glPushMatrix();
+		glTranslatef(0, 7.7, 0);
+		glutSolidCube(1.4);
+	glPopMatrix();
+
+	glColor3f(1., 0., 0.);			//Torso
+	glPushMatrix();
+		glTranslatef(0, 5.5, 0);
+		glScalef(3, 3, 1.4);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glColor3f(0., 0., 1.);			//Right leg
+	glPushMatrix();
+		glTranslatef(-0.8, 4, 0);
+		glRotatef(jumpLegAngle, 0, 0, 1);
+		glTranslatef(0.8, -4, 0);
+		glTranslatef(-0.8, 2.2, 0);
+		glScalef(1, 4.4, 1);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glColor3f(0., 0., 1.);			//Left leg
+	glPushMatrix();
+		glTranslatef(0.8, 4, 0);
+		glRotatef(-jumpLegAngle, 0, 0, 1);
+		glTranslatef(-0.8, -4, 0);
+		glTranslatef(0.8, 2.2, 0);
+		glScalef(1, 4.4, 1);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glColor3f(0., 0., 1.);			//Right arm
+	glPushMatrix();
+		glTranslatef(-2, 6.5, 0);
+		glRotatef(jumpHandAngle, 0, 0, 1);
+		glTranslatef(2, -6.5, 0);
+		glTranslatef(-2, 5, 0);
+		glScalef(1, 4, 1);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glColor3f(0., 0., 1.);			//Left arm
+	glPushMatrix();
+		glTranslatef(2, 6.5, 0);
+		glRotatef(-jumpHandAngle, 0, 0, 1);
+		glTranslatef(-2, -6.5, 0);
+		glTranslatef(2, 5, 0);
+		glScalef(1, 4, 1);
+		glutSolidCube(1);
+	glPopMatrix();
+}
+
+void drawVolleyballRobot(void)
+{
+	glDisable(GL_TEXTURE_2D);
+
+	glColor3f(1., 0.78, 0.06);		//Head
+	glPushMatrix();
+	glTranslatef(0, 7.7, 0);
+	glutSolidCube(1.4);
+	glPopMatrix();
+
+	glColor3f(1., 0., 0.);			//Torso
+	glPushMatrix();
+	glTranslatef(0, 5.5, 0);
+	glScalef(3, 3, 1.4);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glColor3f(0., 0., 1.);			//Right leg
+	glPushMatrix();
+	glTranslatef(-0.8, 4, 0);
+	glRotatef(jumpLegAngle, 0, 0, 1);
+	glTranslatef(0.8, -4, 0);
+	glTranslatef(-0.8, 2.2, 0);
+	glScalef(1, 4.4, 1);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glColor3f(0., 0., 1.);			//Left leg
+	glPushMatrix();
+	glTranslatef(0.8, 4, 0);
+	glRotatef(-jumpLegAngle, 0, 0, 1);
+	glTranslatef(-0.8, -4, 0);
+	glTranslatef(0.8, 2.2, 0);
+	glScalef(1, 4.4, 1);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glColor3f(0., 0., 1.);			//Right arm
+	glPushMatrix();
+	glTranslatef(-2, 6.5, 0);
+	glRotatef(jumpHandAngle, 0, 0, 1);
+	glTranslatef(2, -6.5, 0);
+	glTranslatef(-2, 5, 0);
+	glScalef(1, 4, 1);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glColor3f(0., 0., 1.);			//Left arm
+	glPushMatrix();
+	glTranslatef(2, 6.5, 0);
+	glRotatef(-jumpHandAngle, 0, 0, 1);
+	glTranslatef(-2, -6.5, 0);
+	glTranslatef(2, 5, 0);
+	glScalef(1, 4, 1);
+	glutSolidCube(1);
+	glPopMatrix();
+}
+
+void jumpTimer(int value)
+{
+	if (jumpUpBool && jumpHeight < 10)
+	{
+		jumpHeight++;
+	}
+	else if (jumpUpBool)
+	{
+		jumpUpBool = !jumpUpBool;
+	}
+	else if (!jumpUpBool && jumpHeight > 0)
+	{
+		jumpHeight--;
+	}
+	else if (!jumpUpBool)
+	{
+		jumpUpBool = !jumpUpBool;
+	}
+
+	if (!jumpUpBool && jumpHandAngle < 0)
+	{
+		jumpHandAngle += 10;
+		jumpLegAngle += 5;
+	}
+	else if (jumpUpBool && jumpHandAngle > -180)
+	{
+		jumpHandAngle -= 10;
+		jumpLegAngle -= 5;
+	}
+	glutPostRedisplay();
+	glutTimerFunc(50, jumpTimer, 0);
+}
+
 //---------------------------------------------------------------------
 void initialize(void)
 {
+	loadTexture();
+	glEnable(GL_TEXTURE_2D);
+
 	float grey[4] = { 0.2, 0.2, 0.2, 1.0 };
 	float white[4] = { 1.0, 1.0, 1.0, 1.0 };
 
@@ -206,81 +377,66 @@ void initialize(void)
 //-------------------------------------------------------------------
 void display(void)
 {
-	//float lgt_pos[] = { 0.0f, 50.0f, 0.0f, 1.0f };  //light0 position (directly above the origin)
+	float dir_x = -sin(lookTheta), dir_y = 0, dir_z = -cos(lookTheta);
+	float d = 2;
+	glRotatef(angle_up_down, 0.0, 1.0, 0.0);
+
+	//if (eye_x + dir_x * step + dir_x * d < 14 && eye_x + dir_x * step + dir_x * d > -14 && eye_z + dir_z * step + dir_z * d < 14 && eye_z + dir_z * step + dir_z * d > -14)
+	//{
+		eye_x += dir_x * step;
+		eye_z += dir_z * step;
+	//}
+
+	look_x = eye_x + dir_x * d;
+	look_y = eye_y;
+	look_z = eye_z + dir_z * d;
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(-80, 50, 250, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(eye_x, eye_y, eye_z, look_x, look_y, look_z, 0, 1, 0);
 
 	glLightfv(GL_LIGHT0, GL_POSITION, lgt_src);   //light position
 
 	floor();
-	tracks();
 
 	float shadowMat[16] = { lgt_src[1],0,0,0, -lgt_src[0],0,-lgt_src[2],-1, 0,0,lgt_src[1],0, 0,0,0,lgt_src[1] };
 
 	glEnable(GL_LIGHTING);
 	glPushMatrix();											//Draw Actual Object
 		glRotatef(theta, 0, 1, 0);
-		glTranslatef(0, 1, -120);
+		glTranslatef(0, 1, -50);
 		glRotatef(90, 0, 1, 0);
-		glScalef(5.0, 5.0, 5.0);
 		drawModel();
 	glPopMatrix();
 
-	glDisable(GL_LIGHTING); 
+	glDisable(GL_LIGHTING);
 	glPushMatrix();											//Draw Shadow Object
 		glMultMatrixf(shadowMat);
 		glRotatef(theta, 0, 1, 0);
 		glTranslatef(0, 1, -120);
 		glRotatef(90, 0, 1, 0);
-		glScalef(5.0, 5.0, 5.0);
 		glColor4f(0.2, 0.2, 0.2, 1.0);
 		drawModel();
 	glPopMatrix();
 
+	glEnable(GL_LIGHTING);
+	glPushMatrix();
+		glTranslatef(0, 0, -20);		// put the robot in place
+		glTranslatef(0, jumpHeight, 0);		// make the robot jump
+		drawJumpingRobot();
+	glPopMatrix();
+
 	glutSwapBuffers();   //Useful for animation
-}
-
-void myTimer(int value)
-{
-	theta += 2;
-	glutPostRedisplay();
-	glutTimerFunc(50, myTimer, 0);
-}
-
-void robotTimer(int value)
-{
-	if (movementFlag && robotMovement < 30)
-	{
-		robotMovement++;
-	}
-	else if (movementFlag)
-	{
-		movementFlag = !movementFlag;
-		robotMovement--;
-	}
-	else if (!movementFlag && robotMovement > -30)
-	{
-		robotMovement--;
-	}
-	else
-	{
-		movementFlag = !movementFlag;
-		robotMovement++;
-	}
-	glutPostRedisplay();
-	glutTimerFunc(50, robotTimer, 0);
 }
 
 //--------------------------------------------------------------------------------
 void special(int key, int x, int y)
 {
 	step = 0;
-	if (key == GLUT_KEY_LEFT) theta += 0.1;   //in radians
-	else if (key == GLUT_KEY_RIGHT) theta -= 0.1;
+	if (key == GLUT_KEY_LEFT) lookTheta += 0.1;   //in radians
+	else if (key == GLUT_KEY_RIGHT) lookTheta -= 0.1;
 	else if (key == GLUT_KEY_DOWN) step = -1;
 	else if (key == GLUT_KEY_UP) step = 1;
 
@@ -300,6 +456,7 @@ int main(int argc, char** argv)
 	glutDisplayFunc(display);
 	glutTimerFunc(50, myTimer, 0);
 	glutTimerFunc(50, robotTimer, 0);
+	glutTimerFunc(50, jumpTimer, 0);
 	glutSpecialFunc(special);
 	glutMainLoop();
 	return 0;
